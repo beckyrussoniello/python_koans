6bf1dcde4b408e247c5f45b7655150fb9b5b2a66
Becky Russoniello <rebecca.russoniello@gmail.com>
2021-06-03 19:58:52 -0500

    an alternate solution to about triangle project

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,13 +17,21 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
+    # My first solution:
+    #if unique_side_lengths == 1:
+    #    return 'equilateral'
+    #elif unique_side_lengths == 2:
+    #    return 'isosceles'
+    #else:
+    #    return 'scalene'
+    # Second solution - may be more Pythonic?
+    triangle_types = {
+        1: 'equilateral',
+        2: 'isosceles',
+        3: 'scalene'
+    }
     unique_side_lengths = len(set([a, b, c]))
-    if unique_side_lengths == 1:
-        return 'equilateral'
-    elif unique_side_lengths == 2:
-        return 'isosceles'
-    else:
-        return 'scalene'
+    return triangle_types.get(unique_side_lengths, "Invalid triangle")
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
